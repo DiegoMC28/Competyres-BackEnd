a4e8104d794cc88f033f0d5f10354feaf5a1c252
DiegoMC28 <diego010128@gmail.com>
2023-06-12 21:24:32 +0200

    *

--- API-Competyres/src/index.js
+++ API-Competyres/src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config()
+require("dotenv").config();
 require("./db/mongoose");
 const usuarioRouter = require("./routers/usuario");
 const cocheRouter = require("./routers/coche");
 const valoracionRouter = require("./routers/valoraciones");
 const circuitoRouter = require("./routers/circuito");
 const accionesUsuario = require("./routers/accionesUsuario");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3028;
@@ -19,6 +20,16 @@ app.use(circuitoRouter);
 app.use(valoracionRouter);
 app.use(accionesUsuario);
 
+const public = path.join(__dirname, "../public");
+
+app.use(express.static(public));
+
+app.get("/docs/user", (req, res)=>{res.sendFile(path.join(__dirname, "../docs/user.html"));})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(public, "index.html"));
+});
+
 app.listen(port, () => {
-  console.log("Servidor abierto en el puerto: " + port);
+    console.log("Servidor abierto en el puerto: " + port);
 });
--- competyres/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "./Globals.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./pages/Router";
-
-function App() {
-  return <RouterProvider router={router}></RouterProvider>;
-}
-
-export default App;
